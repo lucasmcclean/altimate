3221a1c697df1dac1069ed4b80ac8b584763ebe1
Arch <rhygonfn@gmail.com>
2025-09-27 12:29:57 -0400

    frontend stuff

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -1,13 +1,9 @@
-import Graph from './components/graph'
 import Switcher from './components/Switcher'
-import nodes from "./tempJson"
 
 function App() {
   return (
     <>
 		<Switcher />
-
-		<Graph nodes={nodes}/>
     </>
   )
 }
